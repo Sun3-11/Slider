6329a66ac77a466d731c26b79bd9a162b9fbe6a8
Sun3-11 <shamsalthri@gmail.com>
2024-12-26 03:06:01 +0300

    )

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Carousel from "./Carousel";
+import SlickCarousel from "./SlickCarousel";
+
+const App = () => {
+  return (
+    <main>
+      <h2>
+        <Carousel />
+        {/* <SlickCarousel /> */}
+      </h2>
+    </main>
+  );
+};
+export default App;
